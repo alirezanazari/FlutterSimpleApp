abd96732cddbc728fec0785fceff0ba80df6324e
Alireza <a.alirezanazari@gmail.com>
2020-03-30 21:17:32 +0430

    Add - Quote list

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterapp/quote.dart';
 
 void main() => runApp(MaterialApp(
-      home: Home(),
+      home: QuoteList(),
     ));
 
 class Home extends StatefulWidget {
@@ -11,7 +12,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   String name = 'Alireza';
 
   @override
@@ -25,14 +25,14 @@ class _HomeState extends State<Home> {
           backgroundColor: Colors.grey[850],
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: (){
+          onPressed: () {
             setState(() {
               name = 'Alireza Nazari';
             });
           },
           backgroundColor: Colors.grey[850],
           child: Icon(
-            Icons.add ,
+            Icons.add,
             color: Colors.white,
           ),
         ),
@@ -58,7 +58,7 @@ class _HomeState extends State<Home> {
                       letterSpacing: 2,
                       fontWeight: FontWeight.normal)),
               SizedBox(height: 10),
-              Text(name ,
+              Text(name,
                   style: TextStyle(
                       color: Colors.amber,
                       fontSize: 18,
@@ -94,4 +94,71 @@ class _HomeState extends State<Home> {
           ),
         ));
   }
+}
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(
+        text: 'Be yourself; everyone else is alredy taken',
+        author: 'Oscar Wilde'),
+    Quote(
+        text: 'I have nothing to declare except my genius',
+        author: 'Oscar Wilde'),
+    Quote(
+        text: 'The truth is rarely pure and never simple',
+        author: 'Oscar Wilde'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: quotes.map((quote) => getQuoteTemplate(quote)).toList(),
+      ),
+    );
+  }
+
+  Widget getQuoteTemplate(Quote quote){
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 14.0 , vertical: 8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text ,
+              style: TextStyle(
+                color: Colors.grey[600],
+                fontSize: 16
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              '- ${quote.author}' ,
+              style: TextStyle(
+                color: Colors.grey[800],
+                fontSize: 12,
+                fontWeight: FontWeight.bold
+              ),
+            )
+          ],
+        ),
+      ) ,
+    );
+  }
+
 }
